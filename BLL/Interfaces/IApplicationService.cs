eaa0a5f7a74247d7d4c8a2cdbf9063e3590b1280
Gods4y <maksim191106@gmail.com>
2026-06-09 10:00:23 +0300

    little fix with IAppServ

--- BLL/Interfaces/IApplicationService.cs
+++ BLL/Interfaces/IApplicationService.cs
@@ -6,6 +6,15 @@ public interface IApplicationService
     Task<OperationResult<ApplicationDto>> ApplyAsync(
         ApplyDto dto, CancellationToken cancellationToken = default);
 
+    Task<OperationResult<ApplicationDto>> ProposeAsync(
+        ProposeDto dto, CancellationToken cancellationToken = default);
+
+    Task<OperationResult<ApplicationDto>> AcceptAsync(
+        int id, int userId, CancellationToken cancellationToken = default);
+
+    Task<OperationResult<ApplicationDto>> RejectAsync(
+        int id, int userId, CancellationToken cancellationToken = default);
+
     Task<IReadOnlyList<VacancyDto>> GetLinkedVacanciesAsync(
         int resumeId, CancellationToken cancellationToken = default);
 
